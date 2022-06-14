d8dd582117b6bb40aa1f0d8d51801e3d53f89a69
Andy Gorbould <gorbould@mail.com>
2022-06-14 16:10:28 +0200

    started with a fresh file & things work quicker :)

--- README.md
+++ README.md
@@ -4,7 +4,16 @@
 1. what is the structure?
 
 2. what are the MAIN purposes?
+    PHP can create interactive content for web pages.
+    PHP can create, access, read and write, erase, and close all files.
+    PHP can extract data from forms.
+    PHP can communicate with cookies.
+    PHP can install, erase, and change data in your database. .......(user's local db or the site's online db?)
+    PHP is used to monitor who has access to what information.
+    PHP can encrypt files.
 
 3. is php a replacement for HTML?
 
 4. is php a replacement for JS?
+
+5. what are the different import form actions/methods?
--- index.php
+++ index.php
@@ -5,98 +5,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AJAX Pokédex</title>
-    <link rel="stylesheet" href="style.css">
+    <title>PHP Pokemon Challenge</title>
 </head>
 
 <body>
-    <div class="everything">
 
-        <img class="logo" src="./img/pokemon_logo.webp" alt="Pokémon">
+    <form action="" method="post">
+        NAME or ID (1-898): <input class="input" id="userNumbInput" type="text" maxlength="12" name="name" value="">
+        <br>
+        Select<input type="submit" id="btnNumbInput" value="submit">
+    </form>
 
-
-        <!-- user input text boxes -->
-        <input class="input" id="userNumbInput" type="text" maxlength="12" placeholder="NAME or ID (1-898)">
-        <button class="input" id="btnNumbInput">Select</button>
-
-
-        <!-- display name -->
-        <div clas="info" id="nameDisplay">
-
-        </div>
-
-
-
-
-        <div class="info">
-            <!-- <p>This has been heavily modified from https://www.youtube.com/watch?v=XL68br6JyYs </p> -->
-        </div>
-
-        <!-- test button -->
-        <!-- <button type="button" onclick="loadDoc()">Change Content</button> -->
-
-
-    </div>
-
-    <!-- PHP Script -->
     <?php
-    // button
-    $dom = new DOMDocument;
-    $run = $dom->getElementById("btnNumbInput");
+    print_r("I selected: " . $_POST['name']); // shows user's input after submitted
 
-    function userNumberInputFunc()
-    {
-        // number (user)
-        $userNumbInputVal = $dom->getElementById("userNumbInput");
-
-        $i = (int)readline($userNumbInputVal);
-        getPokemon($i);
-    };
-
-    $selectedDisplay = $dom->getElementById("nameDisplay");
-    $pokemons_number = 20;
-    $colors = array(
-        "fire:" => '#FDDFDF',
-        "grass:" => '#DEFDE0',
-        "electric:" => '#FCF7DE',
-        "water:" => '#DEF3FD',
-        "ground:" => '#f4e7da',
-        "rock:" => '#d5d5d4',
-        "fairy:" => '#fceaff',
-        "poison:" => '#98d7a5',
-        "bug:" => '#f8d5a3',
-        "dragon:" => '#97b3e6',
-        "psychic:" => '#eaeda1',
-        "flying:" => '#F5F5F5',
-        "fighting:" => '#E6E0D4',
-        "normal:" => '#F5F5F5',
-    );
-
-
-
-
-    // getPokemon = 
-
-    // $url = 'https://pokeapi.co/api/v2/pokemon/' . $id . '';
-    $url = 'https://pokeapi.co/api/v2/pokemon/1';
+    $url = 'https://pokeapi.co/api/v2/pokemon/' . $_POST['name'];
 
     $result = file_get_contents($url);
     $pokemonData = json_decode($result, true);
 
-    // $speciesUrl = ($pokemonData['species']['url']);
-    $speciesUrl = ($pokemonData["species"]["url"]);
+    // $speciesUrl = ($pokemonData["species"]["url"]);
+
+    $pokemonImage = ($pokemonData["sprites"] ["front_default"]); // fetch image url from JSON
+    $pokemonName = ($pokemonData["name"]); // fetch name from JSON
+
+    // var_dump($speciesUrl);
+    // var_dump($pokeImage);
+    print_r("<img src=" . $pokemonImage .  ">"); // image URL inserted
+    print_r("<p>This wee hing is called " . $pokemonName .  "</p>");
 
 
-    var_dump($speciesUrl);
-
-
-
-
-
-
-
+    
 
     ?>
+
+
+<img src="" alt="">
+
+
 </body>
 
 </html>
