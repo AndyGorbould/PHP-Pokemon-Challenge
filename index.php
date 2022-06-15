c65fdf2c8bcb291cb3bb4ed2fc26702d0d72fb0a
Andy Gorbould <lost@MacBook-Air.local>
2022-06-15 16:03:32 +0200

    ugly but functional

--- index.php
+++ index.php
@@ -1,3 +1,81 @@
+<?php
+print_r("I selected ID#: " . $_POST['name']); // shows user's input after submitted
+
+$url = 'https://pokeapi.co/api/v2/pokemon/' . $_POST['name'];
+$result = file_get_contents($url); // fetch json
+$pokemonData = json_decode($result, true); // decode json
+
+
+        $pokemonImage = ($pokemonData["sprites"]["front_default"]); // fetch image url from JSON
+        $pokemonName = ($pokemonData["name"]); // fetch name from JSON
+        $pokemonMoves = ($pokemonData["moves"]);
+
+                // $movesArr = array("");
+                
+                // for ($i=0; $i < 10; $i++) { 
+                //     array_push($movesArr, $pokemonMoves[$i]["move"]);
+                //     print_r("<p>All Moves: " . $movesArr[$i]["name"] .  "</p>");
+                // }
+                    //    $pokemonMovesSingle = ($pokemonMoves[0]["move"]["name"]);
+
+
+
+// SPECIES
+$urlSpecies = ($pokemonData["species"]["url"]); // fetch species api
+$resultSpecies = file_get_contents($urlSpecies);
+$pokemonDataSpecies = json_decode($resultSpecies, true);
+
+
+// EVOLUTION
+$urlSpeciesEvo = ($pokemonDataSpecies["evolution_chain"]["url"]); // fetch evo api
+$resultSpeciesEvo = file_get_contents($urlSpeciesEvo);
+$pokemonDataSpeciesEvo = json_decode($resultSpeciesEvo, true);
+
+
+// EVOLUTION if-loop
+function evolutionList() {
+
+$evoList = array("");
+$evoID = array("");
+if (count($pokemonDataSpeciesEvo["chain"]["evolves_to"]) === 0) {
+    array_push($evoList, $pokemonDataSpeciesEvo["chain"]["species"]["name"]);
+    array_push($evoID, $pokemonDataSpeciesEvo["chain"]["species"]["id"]);
+    // check if 2nd
+    if (count($pokemonDataSpeciesEvo["chain"][0]["evolves_to"]) !== undefined) {
+        array_push($evoList, $pokemonDataSpeciesEvo["chain"][0]["evolves_to"]["species"]["name"]);
+        array_push($evoID, $pokemonDataSpeciesEvo["chain"][0]["evolves_to"]["species"]["url"]);
+        }
+        // check if 3rd
+        if (count($pokemonDataSpeciesEvo["chain"][0]["evolves_to"][0]["evolves_to"]) !== undefined) {
+            array_push($evoList, $pokemonDataSpeciesEvo["chain"][0]["evolves_to"][0]["evolves_to"]["species"]["name"]);
+            array_push($evoID, $pokemonDataSpeciesEvo["chain"][0]["evolves_to"][0]["evolves_to"]["species"]["url"]);
+            }
+
+}
+return $evoList;
+}
+
+
+
+
+
+
+
+print_r("<img src=" . $pokemonImage .  ">"); // image URL inserted
+print_r("<h1>This wee hing is called " . ucwords($pokemonName) .  "</h1>");
+// print_r("<h1>Moves: " . $pokemonMovesSingle .  "</h1>");
+// echo $pokemonMovesSingle;
+
+// abilities:
+// $pokemonData["abilities"]["ability"][0]
+
+
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,31 +94,24 @@
         Select<input type="submit" id="btnNumbInput" value="submit">
     </form>
 
-    <?php
-    print_r("I selected: " . $_POST['name']); // shows user's input after submitted
 
-    $url = 'https://pokeapi.co/api/v2/pokemon/' . $_POST['name'];
-
-    $result = file_get_contents($url);
-    $pokemonData = json_decode($result, true);
-
-    // $speciesUrl = ($pokemonData["species"]["url"]);
-
-    $pokemonImage = ($pokemonData["sprites"] ["front_default"]); // fetch image url from JSON
-    $pokemonName = ($pokemonData["name"]); // fetch name from JSON
-
-    // var_dump($speciesUrl);
-    // var_dump($pokeImage);
-    print_r("<img src=" . $pokemonImage .  ">"); // image URL inserted
-    print_r("<p>This wee hing is called " . $pokemonName .  "</p>");
-
-
+    <!-- <h2 style="color: red;"> <?php echo $evoList; ?> evo</h2>
+    <h2 style="color: red;"> <?php echo $evoList[1]; ?> evo</h2>
+    <h2 style="color: red;"> <?php echo $evoList[2]; ?> evo</h2> -->
     
 
-    ?>
-
-
-<img src="" alt="">
+    <!-- Printing moves from API to HTML :) -->
+    <ul>Moves:
+        <?php
+        $movesArr = array("");
+                    
+                    for ($i=1; $i < 10; $i++) { 
+                        array_push($movesArr, $pokemonMoves[$i]["move"]);
+                        print_r("<li>" . $movesArr[$i]["name"] .  "</li>");
+                    }
+        ?>
+    </ul>
+    <!-- ^^^^^^ -->
 
 
 </body>
